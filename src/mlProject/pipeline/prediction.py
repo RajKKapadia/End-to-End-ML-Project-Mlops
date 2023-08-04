c8fed40b378d7ad23fd172dcc0959d3994dc453a
RajKKapadia <raajforyou@gmail.com>
2023-08-04 18:54:51 +0530

    added type

--- src/mlProject/pipeline/prediction.py
+++ src/mlProject/pipeline/prediction.py
@@ -1,12 +1,13 @@
 import joblib
 from pathlib import Path
 
+import numpy as np
 
 class PredictionPipeline:
     def __init__(self):
         self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
 
-    def predict(self, data):
+    def predict(self, data:np.array):
         prediction = self.model.predict(data)
 
         return prediction
